1a53feaddaa5010cc840edd7c74752bca3768c2d
Blickwinkel1107 <1196061706@qq.com>
2018-08-24 18:09:11 +0800

    yx: 增加SelectFiles界面大小

--- src/test/SelectFiles.java
+++ src/test/SelectFiles.java
@@ -29,6 +29,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import java.awt.Window.Type;
 
 public class SelectFiles extends JFrame {
 
@@ -63,81 +64,30 @@ public class SelectFiles extends JFrame {
 	 * Create the frame.
 	 */
 	public SelectFiles() {
+		setTitle("Select Files Path");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 653, 449);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{97, 234, 68, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{79, 39, 42, 35, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
 
 		lblEnterRequirementPath = new JLabel("Enter requirement path");
-		GridBagConstraints gbc_lblEnterRequirementPath = new GridBagConstraints();
-		gbc_lblEnterRequirementPath.insets = new Insets(0, 0, 5, 5);
-		gbc_lblEnterRequirementPath.anchor = GridBagConstraints.EAST;
-		gbc_lblEnterRequirementPath.gridx = 0;
-		gbc_lblEnterRequirementPath.gridy = 1;
-		contentPane.add(lblEnterRequirementPath, gbc_lblEnterRequirementPath);
 
 		textFieldReq = new JTextField();
-		GridBagConstraints gbc_textFieldReq = new GridBagConstraints();
-		gbc_textFieldReq.insets = new Insets(0, 0, 5, 5);
-		gbc_textFieldReq.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textFieldReq.gridx = 1;
-		gbc_textFieldReq.gridy = 1;
-		contentPane.add(textFieldReq, gbc_textFieldReq);
 		textFieldReq.setColumns(10);
 
 		btnReq = new JButton("Files..");
-		GridBagConstraints gbc_btnReq = new GridBagConstraints();
-		gbc_btnReq.insets = new Insets(0, 0, 5, 5);
-		gbc_btnReq.gridx = 2;
-		gbc_btnReq.gridy = 1;
-		contentPane.add(btnReq, gbc_btnReq);
 
 		lblEnterOldVersion = new JLabel("Enter old version path");
-		GridBagConstraints gbc_lblEnterOldVersion = new GridBagConstraints();
-		gbc_lblEnterOldVersion.insets = new Insets(0, 0, 5, 5);
-		gbc_lblEnterOldVersion.anchor = GridBagConstraints.EAST;
-		gbc_lblEnterOldVersion.gridx = 0;
-		gbc_lblEnterOldVersion.gridy = 2;
-		contentPane.add(lblEnterOldVersion, gbc_lblEnterOldVersion);
 
 		textFieldOldVer = new JTextField();
-		GridBagConstraints gbc_textFieldOldVer = new GridBagConstraints();
-		gbc_textFieldOldVer.insets = new Insets(0, 0, 5, 5);
-		gbc_textFieldOldVer.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textFieldOldVer.gridx = 1;
-		gbc_textFieldOldVer.gridy = 2;
-		contentPane.add(textFieldOldVer, gbc_textFieldOldVer);
 		textFieldOldVer.setColumns(10);
 
 		btnChangeOld = new JButton("Files..");
-		GridBagConstraints gbc_btnChangeOld = new GridBagConstraints();
-		gbc_btnChangeOld.insets = new Insets(0, 0, 5, 5);
-		gbc_btnChangeOld.gridx = 2;
-		gbc_btnChangeOld.gridy = 2;
-		contentPane.add(btnChangeOld, gbc_btnChangeOld);
 
 		lblEnterNewVersion = new JLabel("Enter new version path");
-		GridBagConstraints gbc_lblEnterNewVersion = new GridBagConstraints();
-		gbc_lblEnterNewVersion.insets = new Insets(0, 0, 5, 5);
-		gbc_lblEnterNewVersion.anchor = GridBagConstraints.EAST;
-		gbc_lblEnterNewVersion.gridx = 0;
-		gbc_lblEnterNewVersion.gridy = 3;
-		contentPane.add(lblEnterNewVersion, gbc_lblEnterNewVersion);
 
 		textFieldNewVer = new JTextField();
-		GridBagConstraints gbc_textFieldNewVer = new GridBagConstraints();
-		gbc_textFieldNewVer.insets = new Insets(0, 0, 5, 5);
-		gbc_textFieldNewVer.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textFieldNewVer.gridx = 1;
-		gbc_textFieldNewVer.gridy = 3;
-		contentPane.add(textFieldNewVer, gbc_textFieldNewVer);
 		textFieldNewVer.setColumns(10);
 
 						btnChangeNew = new JButton("Files..");
@@ -145,18 +95,70 @@ public class SelectFiles extends JFrame {
 							public void actionPerformed(ActionEvent e) {
 							}
 						});
-						GridBagConstraints gbc_btnChangeNew = new GridBagConstraints();
-						gbc_btnChangeNew.insets = new Insets(0, 0, 5, 5);
-						gbc_btnChangeNew.gridx = 2;
-						gbc_btnChangeNew.gridy = 3;
-						contentPane.add(btnChangeNew, gbc_btnChangeNew);
 
 						JButton btnConfirm = new JButton("Confirm");
-						GridBagConstraints gbc_btnConfirm = new GridBagConstraints();
-						gbc_btnConfirm.insets = new Insets(0, 0, 5, 5);
-						gbc_btnConfirm.gridx = 1;
-						gbc_btnConfirm.gridy = 5;
-						contentPane.add(btnConfirm, gbc_btnConfirm);
+						GroupLayout gl_contentPane = new GroupLayout(contentPane);
+						gl_contentPane.setHorizontalGroup(
+							gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(36)
+									.addComponent(lblEnterRequirementPath)
+									.addGap(36)
+									.addComponent(textFieldReq, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btnReq))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(41)
+									.addComponent(lblEnterOldVersion)
+									.addGap(41)
+									.addComponent(textFieldOldVer, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btnChangeOld))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(37)
+									.addComponent(lblEnterNewVersion)
+									.addGap(37)
+									.addComponent(textFieldNewVer, GroupLayout.PREFERRED_SIZE, 291, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btnChangeNew))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(340)
+									.addComponent(btnConfirm))
+						);
+						gl_contentPane.setVerticalGroup(
+							gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(148)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(5)
+											.addComponent(lblEnterRequirementPath))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(3)
+											.addComponent(textFieldReq, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addComponent(btnReq))
+									.addGap(15)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(5)
+											.addComponent(lblEnterOldVersion))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(3)
+											.addComponent(textFieldOldVer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addComponent(btnChangeOld))
+									.addGap(11)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(5)
+											.addComponent(lblEnterNewVersion))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(3)
+											.addComponent(textFieldNewVer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addComponent(btnChangeNew))
+									.addGap(38)
+									.addComponent(btnConfirm))
+						);
+						contentPane.setLayout(gl_contentPane);
 	}
 
 	private static void addPopup(Component component, final JPopupMenu popup) {
