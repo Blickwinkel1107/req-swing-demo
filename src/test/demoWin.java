c23fc6dc67d23c1de35983134eecae22886968b4
RatYoung <yhrnju@qq.com>
2018-08-24 14:14:18 +0800

    design login window

--- src/test/demoWin.java
+++ src/test/demoWin.java
@@ -3,10 +3,17 @@ package test;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class demoWin {
 
 	private JFrame frame;
+	private JTextField textField_1;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,32 @@ public class demoWin {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
 
+		JPanel panel = new JPanel();
+		panel.setBounds(52, 43, 341, 36);
+		frame.getContentPane().add(panel);
+
+		JLabel lblUsername = new JLabel("username:");
+		panel.add(lblUsername);
+
+		textField_1 = new JTextField();
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(52, 91, 341, 36);
+		frame.getContentPane().add(panel_1);
+
+		JLabel lblPassword = new JLabel("password:");
+		panel_1.add(lblPassword);
+
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(155, 139, 117, 29);
+		frame.getContentPane().add(btnLogin);
+	}
 }
